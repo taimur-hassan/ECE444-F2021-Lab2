7fe6004cc74df20730656ea5f22f93af2cf6c481
Taimur Hassan <taimur9009@gmail.com>
2021-09-26 05:28:33 -0400

    Completed activity 4. Added UofT email field to form on home page.

--- hello.py
+++ hello.py
@@ -4,14 +4,15 @@ from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Taimur'
+app.config['SECRET_KEY'] = 'Stranger'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -23,9 +24,13 @@ def index():
         old_name = session.get('name')
         if (old_name is not None and old_name != form.name.data):
             flash('Looks like you changed your name!')
+        old_email = session.get('email')
+        if (old_email is not None and old_email != form.email.data):
+            flash('Looks like you changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
